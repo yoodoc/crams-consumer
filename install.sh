bf8e9b58572a39a99bb5dd87e99d8616caa8d617
송유덕 <yoodoc@song-yudeok-MacBook-Air.local>
2014-02-24 10:16:37 +0900

    modify project run&install script

--- install.sh
+++ install.sh
@@ -1,12 +1,46 @@
 #!/bin/bash
 
+ORIGIN_DIR=$(pwd)
+uname=$(uname -a|grep -i 'ubuntu')
+if [ "$uname" ];
+then
+    $PROJECT_DIR=$(readlink -f $0|xargs dirname)
+else
+    echo this scipt need ubuntu environment
+    exit
+fi
+
+cd $PROJECT_DIR
 mvn clean
-
+mvn install:install-file -Dfile=$PROJECT_DIR/src/main/lib/kafka_2.8.0-0.8.0.jar -DgroupId=com.ktcloudware.kafka -DartifactId=kafka_2.8.0 -Dversion=0.8.0 -Dpackaging=jar
 #mvn install:install-file -Dfile=$PWD/src/main/lib/kafka_2.8.0-0.8.0-beta1.jar -DgroupId=com.ktcloudware.kafka -DartifactId=kafka_2.8.0 -Dversion=0.8.0-beta1 -Dpackaging=jar
-
 #mvn install:install-file -Dfile=src/main/lib/kafka-assembly-0.8.0-beta1-deps.jar -DgroupId=com.ktcloudware.kafka -DartifactId=kafka_assembly -Dversion=0.8.0-beta1 -Dpackaging=jar
-
-mvn install:install-file -Dfile=$PWD/src/main/lib/kafka_2.8.0-0.8.0.jar -DgroupId=com.ktcloudware.kafka -DartifactId=kafka_2.8.0 -Dversion=0.8.0 -Dpackaging=jar
-
+mvn install:install-file -Dfile=$PROJECT_DIR/src/main/lib/kafka_2.8.0-0.8.0.jar -DgroupId=com.ktcloudware.kafka -DartifactId=kafka_2.8.0 -Dversion=0.8.0 -Dpackaging=jar
 mvn assembly:assembly -DskipTests
+
+cd $ORIGIN_DIR
+#set config for each server environment
+case "$1" in
+    develop)
+        #set configuration for develop environment
+        cp $PROJECT_DIR/conf/*.properites.dev $PROJECT_DIR/conf/*.properties
+        for i in `ls $PROJECT_DIR/conf|grep dev`
+        do
+            #name=$(echo $i|awk -F . '/.dev/{print $1"."$2}')
+            name=$(echo $i|sed 's/properties.dev/properties/')
+            cp $PROJECT_DIR/$i $PROJECT_DIR/$name
+        done
+        for i in `ls $PROJECT_DIR/bin|grep dev`
+        do
+            name=$(echo $i|sed 's/sh.dev/properties/')
+            cp $PROJECT_DIR/$i $PROJECT_DIR/$name
+        done
+        exit 1;;
+    staging)
+        exit 1;;
+    product)
+        exit 1;;
+    *)
+        exit 1;;
+esac
 
